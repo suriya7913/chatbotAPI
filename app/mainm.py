e28328f1255e4d304e624538ca1d8053d5922c82
suriya7913 <suriya962927@gmail.com>
2022-06-27 00:33:14 +0530

    mainm

---
+++ app/mainm.py
@@ -0,0 +1,15 @@
+#app/mainm.py
+
+from flask import Flask
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+  
+        return jsonify(data)
